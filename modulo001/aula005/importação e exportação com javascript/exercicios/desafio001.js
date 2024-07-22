ac01a8e9ef90deaf15affeef302d7818522d2e56
GabrielEurico <gabriel.eurico99@outlook.com>
2024-07-22 14:24:44 -0300

    update desafio

--- modulo001/aula005/importação e exportação com javascript/exercicios/desafio001.js
+++ modulo001/aula005/importação e exportação com javascript/exercicios/desafio001.js
@@ -1,12 +1,11 @@
-const {gets, print } = require('./funcoes-auxiliares-ex0001');
+const { gets, print } = require("./funcoes-auxiliares-ex0001");
 
-const media = gets()
+const media = gets();
 
-if(media < 5 ){
-    print('Reprovado')
-}else if(media >=5 && media <7){
-    print('Recuperação')
-}else{
-    print('Aprovado')
+if (media < 5) {
+  print("Reprovado");
+} else if (media >= 5 && media < 7) {
+  print("Recuperação");
+} else {
+  print("Aprovado");
 }
-
